2032a32861b1843b771bfb28bf8cc809f067fafd
samar0507 <samar.hamdi@esprit.tn>
2021-04-10 16:43:23 -0700

    hey

---
+++ ennemi/ennemi.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_ttf.h>
+
+
+
+typedef struct ennemi
+{
+SDL_Rect position_entite;
+SDL_Surface *affichage_secondaire[20];
+
+int speed;
+int sens;
+int Frame;
+	
+	
+
+
+}ennemi;
+//int collision_enn(personnage *p, ennemi en);
+/*----- ennemi 1-----*/
+void initialiser_ennemi1(ennemi *en1);
+void afficher_ennemi1(ennemi en1,SDL_Surface *ecran);
+void animation1(ennemi *en1,int a,int b);
+void deplacement( ennemi * en1,int* a,int *b);
+
+/*----- ennemi 2-----*/
+void initialiser_ennemi2(ennemi *en2);
+void afficher_ennemi2(ennemi en2, SDL_Surface *ecran);
+void animation2(ennemi *en2,int a,int b);
+void deplacement( ennemi * en2,int* a,int *b);
+
